de8f19b8560c74e026fb1a00aa027a6b26b6cb5b
Comnarin <Nsirinapuk@gmail.com>
2022-10-04 23:05:59 +0700

    navbar for mobile

--- src/navbar.js
+++ src/navbar.js
@@ -1,9 +1,21 @@
 import { Link } from "react-router-dom"
+const navToggle = document.querySelector('#navToggle');
+const nav = document.querySelector('nav');
+
+navToggle.addEventListener("click",() =>{
+    nav.classList.toggle('open')
+}
+
+)
 export default function Navbar(){
     return(
         <div class = 'navcontainer'>
             <div>
                 <h2>logo</h2>
+                <div className = "nav-toggle" id = "navToggle">
+                    <img src="https://www.richardmiddleton.me/wp-content/themes/richardcodes/assets/img/hamburger.svg" alt="hamburger menu"/>
+
+                </div>
             </div>
             <nav className ="nav">  
                 <ul>
