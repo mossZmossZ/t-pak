2a45f225a5a80b61ffb9b3d2a50caa109883fdcb
Comnarin <Nsirinapuk@gmail.com>
2022-10-05 16:11:18 +0700

    update

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,20 +1,23 @@
 import"./contact.css"
+import Line from "../pictures/LINE_icon.png"
 export default function Contact(){
     return (
         
     <div>
         <div className="header" >
-                Contact US    
+                CONTACT US    
         </div>
         <div className="background">
             <div className="facebook">
+                
                 Facebook : T-pak
+                
             </div>
             <div className="line">
-                Line: T-pak
+                <img src={Line}></img> Line : T-pak
             </div>
             <div className="call">
-                Call: 088-753-0291
+                Call : 088-753-0291
             </div>
             
         </div>
