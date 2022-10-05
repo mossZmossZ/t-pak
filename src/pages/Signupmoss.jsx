3d2ffdcc592e2b3ba7accb70b835c0b1a63d616c
Nattavee Narischat <nattavee123vee@gmail.com>
2022-10-05 14:57:37 +0700

    signup validation update
    special character error

--- src/pages/Signupmoss.jsx
+++ src/pages/Signupmoss.jsx
@@ -1,23 +1,94 @@
 import "./Signupmoss.css"
 import { useState } from "react";
 import FormInput from "./Signupcomponents/FormInput";
+import { editableInputTypes } from "@testing-library/user-event/dist/utils";
 const Signupmoss = () =>{
-    const [username,setUsername] = useState("")
+    const [values,setValues] = useState({
+        username:"",
+        email:"",
+        birthday:"",
+        password:"",
+        confirmPassword:"",
+    });
 
-    console.log(username)
+    const inputs =[
+        {
+            id: 1,
+            name: "username",
+            type: "text",
+            placeholder: "Username",
+            /*errorMessage:
+                "Username should be 3-16 characters and shouldn't include any special character!",
+            label: "Username",
+            pattern: "^[A-Za-z0-9]{3,16}$",*/
+            required: true,
+        },
+        {
+            id: 2,
+            name: "email",
+            type: "email",
+            placeholder: "Email",
+            errorMessage: "It should be a valid email address!",
+            label: "Email",
+            required: true,
+        },
+        {
+            id: 3,
+            name: "birthday",
+            type: "date",
+            placeholder: "Birthday",
+            label: "Birthday",
+        },
+        {
+            id: 4,
+            name: "password",
+            type: "password",
+            placeholder: "Password",
+            /*errorMessage:
+                "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
+            label: "Password",
+            pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,*/
+            required: true,
+        }
+        ,
+        {
+            id: 5,
+            name: "confirmPassword",
+            type: "password",
+            placeholder: "Confirm Password",
+            errorMessage: "Passwords don't match!",
+            label: "Confirm Password",
+            pattern: values.password,
+            required: true,
+        }
+        
+    ]
 
-    console.log("re-randered")
+    const handleSubmit =(e)=>{
+        e.prevenDefault();
+    };
 
+    const onChange =(e)=>{
+        setValues({...values,[e.target.name]: e.target.value});
+    }
+
+    //console.log(values);
     return (
+   
     <div className = "SignupmossApp">
-        <form>
-            <FormInput placeholder="Username" setUsername={setUsername}/>
-            <FormInput placeholder="Email"/>
-            <FormInput placeholder="Full Name"/>
-            <FormInput placeholder="something else."/>
-
-        </form>
+        <form onSubmit={handleSubmit}>
+        <h1>Register</h1>
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
+        <button>Submit</button>
+      </form>
     </div>
-    );
-}
+  );
+};
 export default Signupmoss;
