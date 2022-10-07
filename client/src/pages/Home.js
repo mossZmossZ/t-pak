e8b3aed4f1a4e47992ba772c845fac4691074f09
Comnarin <Nsirinapuk@gmail.com>
2022-10-08 01:56:33 +0700

    boostrap

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,3 +1,11 @@
 export default function Home(){
-    return <h1>Home</h1>
+    return (
+         <div className="container">
+            <botton className="btn btn-primary">
+                พระจอมเกล้าพระนครเหนือ
+            </botton>
+            
+         </div>
+    )
+    
 }
