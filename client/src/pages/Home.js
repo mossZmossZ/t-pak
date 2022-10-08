723635d23c69ba0f0d8755fdf9eb620d61e07333
Comnarin <Nsirinapuk@gmail.com>
2022-10-08 16:57:04 +0700

    home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,24 @@
+import kmutnb from "../pictures/kmutnb.jpg"
+import './home.css'
+import { Link, useLinkClickHandler } from "react-router-dom"  
 export default function Home(){
     return (
          <div className="container">
-            <botton className="btn btn-primary">
-                พระจอมเกล้าพระนครเหนือ
+            <h1>ซื้อขายสัญญาหอพัก หารูมเมท</h1>
+            <botton className="btn-kmutnb">
+                <ul>พระจอมเกล้าพระนครเหนือ</ul>
+                <img src={kmutnb} className="kmutnb"></img> 
+                <Link to ></Link>   
+            </botton>
+           
+            <botton className="btn-kmitl">
+                <ul>พระจอมเกล้าลาดกระบัง</ul>
+                <img src={kmutnb} className="kmuitl"></img>    
+            </botton>
+           
+            <botton className="btn-kmutt">
+                <ul>พระจอมเกล้าธนบุรี</ul>
+                <img src={kmutnb} className="kmutt"></img>    
             </botton>
             
          </div>
