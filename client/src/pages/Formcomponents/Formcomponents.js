04117e7f5e07e1a8adb2a01c05e4f2cf17429185
Nattavee Narischat <nattavee123vee@gmail.com>
2022-10-12 23:29:20 +0700

    update font baloo2

--- client/src/pages/Formcomponents/Formcomponents.js
+++ client/src/pages/Formcomponents/Formcomponents.js
@@ -3,7 +3,9 @@ import"./Formcomponents.css";
 const FormComponents=()=>{
     return(
         <div className="form_container">
-            <h1>Form Component</h1>
+            <div id="mossZ">
+                <h1>Form Component</h1>
+            </div>
         </div>
     );
 }
