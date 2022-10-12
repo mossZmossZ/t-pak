841c62546d2e5c54aae5d3f317097dc6299c29bf
Nattavee Narischat <nattavee123vee@gmail.com>
2022-10-13 03:00:14 +0700

    Update Server Blogs

--- Server/routes/blog.js
+++ Server/routes/blog.js
@@ -1,10 +1,10 @@
 const express = require("express")
 const router = express.Router()
 const {create} = require('../controllers/blogController')
-
+const {getAllblogs} = require('../controllers/blogController')
 
 
 router.post('/create',create)
-
+router.get('/blogs',getAllblogs)
 
 module.exports = router
--- client/src/pages/Formcomponents/Formcomponents.js
+++ client/src/pages/Formcomponents/Formcomponents.js
@@ -1,11 +1,58 @@
-
+import {useState} from "react";
 import"./Formcomponents.css";
 import"../fonts/Baloo2-Regular.ttf";
+import axios from "axios";
+import Swal from "sweetalert2";
+
+
 const FormComponents=()=>{
+    const [state,setState] = useState({
+        title:"",
+        content:"",
+        author:""
+    })
+    const {title,content,author} = state
+
+    //กำหนดค่าให้กับ state
+    const inputValue =name=>event=>{
+        //console.log(name,"=",event.target.value)
+        setState({...state,[name]:event.target.value});
+    }
+    const submitForm=(e)=>{
+        e.preventDefault();
+        //console.table({title,content,author})
+        //console.log("API URL = ",process.env.REACT_APP_API)
+        axios
+        .post(`${process.env.REACT_APP_API}/create`,{title,content,author})
+        .then(response=>{
+            Swal.fire(
+                'แจ้งเตือน','บันทึกสำเร็จ!','success')
+                setState({...state,title:"",content:"",author:""})
+        }).catch(err=>{
+            Swal.fire({icon: 'error',title: 'Oops...',text: err.response.data.error,footer: '<a href="">Why do I have this issue?</a>'})
+            //alert(err.response.data.error)
+        })
+    }
     return(
         <div className="form_container">
             <div>
-                <h1>เขียนบทความ</h1>
+                <h1>เขียนข้อความ</h1>
+                
+                <form onSubmit={submitForm}>
+                    <div className="form-group">
+                        <label>ชื่อบทความ </label>
+                        <input type="text" classname="form-control" value={title} onChange={inputValue("title")}/>
+                    </div>
+                    <div className="form-group">
+                        <label>รายละเอียด </label>
+                        <textarea classname="form-control" value={content} onChange={inputValue("content")}></textarea>
+                    </div>
+                    <div className="form-group">
+                        <label>ผู้แต่ง </label>
+                        <input type="text" classname="form-control" value={author} onChange={inputValue("author")}/>
+                    </div>
+                    <input type="submit" value="บันทึก" className="btn btn-primary"></input>
+                </form>
             </div>
         </div>
     );
