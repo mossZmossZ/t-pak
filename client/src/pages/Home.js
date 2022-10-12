366a5f805ad962b0a83230e5944ec73d5cddb488
Comnarin <Nsirinapuk@gmail.com>
2022-10-12 23:28:47 +0700

    home update

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,7 @@
 import kmutnb from "../pictures/LOGO-KMUTNB.png"
+import kmitl from "../pictures/kmitl-logo.png"
+import kmutt from "../pictures/Kmutt-logo.png"
+import Tu from "../pictures/TU_logo.png"
 import './home.css'
 import {Link} from "react-router-dom"  
 export default function Home(){
@@ -11,33 +14,33 @@ export default function Home(){
                 
                     <div className="btn-kmutnb">
                         <Link to ="/kmutnb">
-                            <img src={kmutnb} className="kmutnb"></img>
+                            <img src={kmutnb} className="kmutnb" alt=""></img>
                             <p>KMUTNB</p>
                             <p>พระจอมเกล้าพระนครเหนือ</p>
                         </Link>
                     </div>
-                    <div className="btn-kmutnb">
-                        <Link to ="/kmutnb">
-                            <img src={kmutnb} className="kmutnb"></img>
-                            <ul>KMUTNB</ul>
-                            <p>พระจอมเกล้าพระนครเหนือ</p>
+                    <div className="btn-kmitl">
+                        <Link to ="/kmitl">
+                            <img src={kmitl} className="kmitl" alt=""></img>
+                            <p>KMITL</p>
+                            <p>พระจอมเกล้าลาดกระบัง</p>
                         </Link>
                     </div>
                 </div>
             
                 <div className="btn-container2">
-                    <div className="btn-kmutnb">
-                        <Link to ="/kmutnb">
-                            <img src={kmutnb} className="kmutnb"></img>
-                            <p>KMUTNB</p>
-                            <p>พระจอมเกล้าพระนครเหนือ</p>
+                    <div className="btn-kmutt">
+                        <Link to ="/kmutt">
+                            <img src={kmutt} className="kmutt" alt=""></img>
+                            <p>KMUTT</p>
+                            <p>พระจอมเกล้าธนบุรี</p>
                         </Link>
                     </div>
-                    <div className="btn-kmutnb">
-                        <Link to ="/kmutnb">
-                            <img src={kmutnb} className="kmutnb"></img>
-                            <p>KMUTNB</p>
-                            <p>พระจอมเกล้าพระนครเหนือ</p>
+                    <div className="btn-tu">
+                        <Link to ="/tu">
+                            <img src={Tu} className="tu" alt=""></img>
+                            <p>TU</p>
+                            <p>ธรรมศาสตร์</p>
                         </Link>
                     </div>
 
