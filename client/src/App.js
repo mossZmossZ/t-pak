91cd078b1e83c0b765a6e6f195986a614850b045
Nattavee Narischat <nattavee123vee@gmail.com>
2022-10-12 23:12:53 +0700

    update form exercise

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,11 @@ import Login from "./pages/Login"
 import Home from './pages/Home';
 import {Route,Routes} from "react-router-dom"
 import Signup from './pages/Signup.jsx';
+<<<<<<< Updated upstream
 import Kmutnb from './pages/kmutnb_page';
+=======
+import Formcomponents from './pages/Formcomponents/Formcomponents.js';
+>>>>>>> Stashed changes
 
 function App() {
   return(
@@ -16,7 +20,11 @@ function App() {
           <Route path='/contact'element={<Contact/>}/>
           <Route path='/login'element={<Login/>}/>
           <Route path='/signup'element={<Signup/>}/>
+<<<<<<< Updated upstream
           <Route path='/kmutnb'element={<Kmutnb/>}/>
+=======
+          <Route path='/Form'element={<Formcomponents/>}/>
+>>>>>>> Stashed changes
         </Routes>
       
       </div>
---
+++ client/src/pages/Formcomponents/Formcomponents.js
@@ -0,0 +1,10 @@
+
+import"./Formcomponents.css";
+const FormComponents=()=>{
+    return(
+        <div className="form_container">
+            <h1>Form Component</h1>
+        </div>
+    );
+}
+export default FormComponents;
