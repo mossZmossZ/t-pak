a3fb32373fa9f8cea947011ecf0357420dae6d86
Nattavee Narischat <nattavee123vee@gmail.com>
2022-10-19 19:27:31 +0700

    update Topic show

--- client/src/pages/Kmutnb/kmutnb_location.js
+++ client/src/pages/Kmutnb/kmutnb_location.js
@@ -32,9 +32,9 @@ const Kmutnblocation=()=>{
             <hr></hr>
             {kmutnblocations.map((kmutnblocation,index)=>(
             <div className="row" key={index} style={{borderBottom:'1px solid silver'}}>
-                <div className="col pt-3 pb-2" key={index} id="formcontainer">
+                <div className="column" key={index} id="formcontainer">
                     <div className="headcontent">
-                        <h2>{kmutnblocation.Name}</h2>
+                        <h2>{kmutnblocation.name}</h2>
                     </div>
                     <p>รายละเอียด : {kmutnblocation.detail.substring(0,180)}</p>
                     <p className="text-muted">ราคา : {kmutnblocation.price} , เบอร์โทรศัพท์: {kmutnblocation.telephone}</p>
